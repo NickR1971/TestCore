f0467c7f302a2ca438109d4128aa8393b4111455
Nick R <nro1912@gmail.com>
2021-08-31 10:37:02 +0300

    localization

---
+++ Assets/Scripts/Localization/CLocalisation.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum UsedLocal { english = 0, ukrainian = 1 }
+
+public static class CLocalisation
+{
+    public static event Action reloadText;
+
+    private static SortedList<string, string> uiString;
+
+    public static bool Init()
+    {
+        reloadText = null;
+        if (uiString != null) return false;
+        
+        uiString = new SortedList<string, string>();
+        return true;
+    }
+
+    public static void LoadLocalPrefab(GameObject _localPrefab)
+    {
+        CLocal local;
+        GameObject loc = MonoBehaviour.Instantiate(_localPrefab);
+        uiString.Clear();
+        local = loc.GetComponent<CLocal>();
+        local.Init(uiString);
+        MonoBehaviour.Destroy(loc);
+        reloadText?.Invoke();
+    }
+
+    public static string GetString(string _key)
+    {
+        if (uiString.TryGetValue(_key, out string value))
+            return value;
+            
+        return $"<<empty key[{_key}]>>";
+    }
+    public static string GetString(ELocalStringID _id)
+    {
+        return GetString(_id.ToString());
+    }
+}
---
+++ Assets/Scripts/Localization/CTextLocalize.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+
+public class CTextLocalize : MonoBehaviour
+{
+    private Text textField;
+    [SerializeField] public string strID;
+
+    private void Start()
+    {
+        textField = GetComponent<Text>();
+        RefreshText();
+        CLocalisation.reloadText += RefreshText;
+    }
+
+    private void OnDestroy()
+    {
+        CLocalisation.reloadText -= RefreshText;
+    }
+
+    public void RefreshText()
+    {
+        textField.text = CLocalisation.GetString(strID);
+    }
+}
