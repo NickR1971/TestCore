b4924904bc816de3ae734d521efa700a8af38dba
Nick R <nro1912@gmail.com>
2021-09-06 17:36:21 +0300

    Game console added

---
+++ Assets/Scripts/interfaces/IGameConsole.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public interface IGameConsole : IService
+{
+    void ShowMessage(string _msg);
+    void AddCommand(CGameConsoleCommand _command);
+    void Show();
+    void Hide();
+}
