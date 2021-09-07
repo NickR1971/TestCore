4ab6bd6d325f49582bac3166d070258e0513a90d
Nick R <nro1912@gmail.com>
2021-09-07 19:05:36 +0300

    Settings and main menu added

---
+++ Assets/Scripts/UI/CMainMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CMainMenu : CMenu
+{
+    private Button loadButton = null;
+    ISaveLoad iSaveLoad;
+    void Start()
+    {
+        InitMenu();
+        iSaveLoad = AllServices.Container.Get<ISaveLoad>();
+
+        AddButton(ELocalStringID.core_quit).onClick.AddListener(QuitGame);
+    }
+    private void OnEnable()
+    {
+        iSaveLoad = AllServices.Container.Get<ISaveLoad>();
+        if (loadButton != null)
+        {
+            loadButton.interactable = iSaveLoad.IsSavedGameExist();
+        }
+    }
+
+    public void QuitGame()
+    {
+        iMainMenu.Quit();
+    }
+
+}
+
